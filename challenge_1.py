53fe2ddee8ebe2530308df84120f9aa435729e49
Sean <smorris@ausdk12.org>
2024-03-27 10:49:04 -0700

    update challenge 1

--- challenge_1.py
+++ challenge_1.py
@@ -3,9 +3,6 @@ from time import sleep
 from drv8833 import DRV8833 # motor control
 from vl53l4cd import VL53L4CD # distance Sensor
 
-motor_ctl = None
-distance_sensor = None
-
 def flash_led():
     print("Flashing Lights! - Challenge 1")
     led = Pin("LED")
@@ -13,8 +10,7 @@ def flash_led():
     sleep(0.5)
     led.off()
 
-def configure_motor_ctl():
-    global motor_ctl
+def create_motor_ctl():
     print("Confgiure Motor")
     frequency = 40000
     ain1 = PWM(Pin(15, Pin.OUT))
@@ -26,25 +22,20 @@ def configure_motor_ctl():
     ain2.freq(frequency)
     bin1.freq(frequency)
     bin2.freq(frequency)
-    motor_ctl = DRV8833(ain1, ain2, bin1, bin2)
+    return DRV8833(ain1, ain2, bin1, bin2)
 
-def configure_distance_sensor():
-    global distance_sensor
+def create_distance_sensor():
     print("Confgiure Sensor")
     distance_sensor_i2c = SoftI2C(sda=Pin(0), scl=Pin(1))
-    distance_sensor = VL53L4CD(distance_sensor_i2c)
+    return VL53L4CD(distance_sensor_i2c)
 
 # run some code!
 flash_led()
-configure_motor_ctl()
-configure_distance_sensor()
+motor_ctl = create_motor_ctl()
+distance_sensor = create_distance_sensor()
 motor_ctl.throttle_a(1)
 distance_sensor.start_ranging()
 while True:
-    while not distance_sensor.data_ready:
-        pass
-    distance_sensor.clear_interrupt()
-
     print("Distance: {} cm".format(distance_sensor.distance))
     if distance_sensor.distance < _____:
         motor_ctl.throttle_a(1)
@@ -52,5 +43,5 @@ while True:
         motor_ctl.throttle_a(0.5)
     elif distance_sensor.distance < _____:
         motor_ctl.stop_a()
-
+    sleep(0.05)
 
